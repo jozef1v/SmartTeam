aff93e424774a08a422edf6f9da12022d0e68d88
Jozef Vargan <xvarganj@stuba.sk>
2022-10-03 07:22:36 +0200

    Pridanie opisu funkcie. Zadávanie konštánt riadenia bude potrebné vyňať do hlavného skriptu a zadávať nanovo v každom samplingu (zadanie modifikovaných konštánt adminom má na starosti Monika).

--- PID.m
+++ PID.m
@@ -1,7 +1,24 @@
-function [u] = PID(e,e_p,u_p)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% PID
+%
+% PID temperature controller of the Vesna greenhouse. M-file containing
+% the external function contains the values of the current 'e' and
+% the previous 'e_p' control error, as well as the previous value of
+% the control output 'u_p' as an input argument. The output from this
+% function is the actual controlled output 'u' for the given signal
+% sampling period.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function u = PID(e,e_p,u_p)
+
+    % Controller constants
     Z_r = 45;
     T_i = 6;
     T_s = 1;
+
+    % Control output
     u_d = Z_r*(e-e_p)+ Z_r/T_i*T_s*e;
     u = u_p+u_d;
 end
