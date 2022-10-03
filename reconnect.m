80e7dc73edbcc341c2bd4b2ec27d45307c9234c6
Jozef Vargan <xvarganj@stuba.sk>
2022-10-03 07:28:05 +0200

    Pridanie opisu funkcie a špecifikácia jednotlivých credentials. Ak sa nebude vedieť connectnúť, bude ho to skúšať connectnúť do aleluja, až kým sa nepripojí. Čas na odozvu Arduino Cloudu sa zvýšil na 10 sekúnd (aby mal dostatok času na lognutie sa).

--- reconnect.m
+++ reconnect.m
@@ -1,23 +1,59 @@
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% reconnect
+% 
+% File to login to Arduino Cloud Storage. M-file consists of an external
+% function that does not require input parameters. It provides
+% the 'options' parameter which contains the basic settings for MATLAB's
+% communication with the Arduino Cloud web server.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function options = reconnect
+
+%{
+    Arduino Cloud credentials
+    - requires:
+        - client_id     - client identifier
+        - client_secret - client secret (password)
+        - url           - url to web sevice
+%}
 client_id = "Ij7686MXMIXCS4JyGJTe5nyQNQM6w7R9";
 client_secret = "BWbnmLopN6lUDPncNZ5zDmAYvHk81TQUbHWFACt9FwysSfqiBgQYhpkVeIYLtYDQ";
 url = "https://api2.arduino.cc/iot/v1/clients/token";
-headerFields = {'Content-type' 'application/x-www-form-urlencoded';'charset' 'UTF-8'};
+
+%{
+    Access specification
+    - options:
+        - headerFields
+            - content-type - specifies type of header content
+            - charset - character set
+        - contentType - data format
+        - mediaType - type of data send to webserver
+        - timeout - 
+    - webwrite:
+        - API credentials
+            - url, client_id, client_secret
+        - grant_type - specificy type of Arduino Cloud access
+        - audience - url to web page
+        
+%}
+headerFields = {'Content-type','application/x-www-form-urlencoded';'charset','UTF-8'};
 options = weboptions('HeaderFields', headerFields);
 
+% Arduino Cloud connection response
+try
+    response = webwrite(url,'client_id', client_id,'client_secret', ...
+        client_secret,'grant_type','client_credentials','audience', ...
+        'https://api2.arduino.cc/iot',options);
+catch
+    errors('connect');
+end
 
-response = webwrite(url,'client_id', client_id,'client_secret', client_secret,'grant_type','client_credentials','audience','https://api2.arduino.cc/iot',options);
-
-
-
-%data = [...
-% '&client_id=', client_id,...
-% '&client_secret=', client_secret
-% ];
-%response = webwrite(url,data);
+% Response access token
 access_token = response.access_token;
-% save access token for future calls
-%propertyValue = {'propertyValue', ['value', 20]};
-headerFields = {'Authorization', ['Bearer ', access_token]};
-%options = weboptions('HeaderFields', propertyValue);
-options = weboptions('HeaderFields', headerFields, 'ContentType','json','MediaType', 'auto');
-% 'MediaType', 'application/json'
+
+% Bearer token for authorization
+headerFields = {'Authorization',['Bearer ',access_token]};
+options = weboptions('HeaderFields',headerFields,'ContentType','json', ...
+    'MediaType','auto','Timeout',10);
