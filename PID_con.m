839dfab7005b4c6091dc31a500ad5c4129df061e
Jozef Vargan <xvarganj@stuba.sk>
2022-10-07 15:36:06 +0200

    Saturácia akčného zásahu, rovnako tak aj resetovanie regulačnej odchýlky a akčného zásahu v k-1 vzorke boli z heatM.m presunuté sem.

--- PID_con.m
+++ PID_con.m
@@ -11,7 +11,7 @@
 % sampling period.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function u = PID_con(e,e_p,u_p)
+function [u,u_pN,e_pN] = PID_con(e,e_p,u_p)
 
     % Controller constants
     Z_r = 45;
@@ -21,6 +21,17 @@ function u = PID_con(e,e_p,u_p)
     % Control output
     u_d = Z_r*(e-e_p)+ Z_r/T_i*T_s*e;
     u = u_p+u_d;
+
+    % Control output saturation
+    if u > 255
+        u = 255;
+    elseif u < 0 
+        u = 0;
+    end
+
+    % Reset control error & output (e(k)->e(k-1) & u(k)->u(k-1))
+    u_pN = u;
+    e_pN = e;
 
 % u(k) = (Kp + Ki*Δt + Kd/Δt) * e(k) - (Kp + 2*Kp/Δt) * e(k-1) +
 %             + Kd/Δt * e(k-2) + u(k-1)
