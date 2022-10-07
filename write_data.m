e2ca826fff6bd91bbca2fa856a268d598b95e7fc
Jozef Vargan <xvarganj@stuba.sk>
2022-10-07 15:34:03 +0200

    Spracováva query pre posielanie dát, nech kód nie je 300-krát napísaný v send_data.m

---
+++ write_data.m
@@ -0,0 +1,22 @@
+
+function write_data(pid,id,data,error_key,options)
+
+% Initialize counter 'spec' & request method
+spec = 0;
+options.RequestMethod = 'put';
+
+% Send heating off data
+while(true)
+    try
+        webwrite(strcat("https://api2.arduino.cc/iot/v2/things/{", ...
+            device(pid),"}/properties/{",d_type(id),"}/publish"), ...
+            data,options);
+        break
+    catch
+        spec = spec + 1;
+        options = errors(error_key);
+        options.RequestMethod = 'put';
+    end
+end
+
+end
