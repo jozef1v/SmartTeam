937967f47ee8136d35315ac4c4d63295983d1cb0
Jozef Vargan <xvarganj@stuba.sk>
2022-10-07 15:37:58 +0200

    Detekcia anomálií bola upravená do obdobnej štruktúry ako je posielanie a sťahovanie dát - cez externú funkciu, tvoriacu samotné query.

--- anomalies.m
+++ anomalies.m
@@ -9,85 +9,59 @@
 % errors (anomalies) associated with Vesna control.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function optionsN = anomalies(T_avg,t_max,fan_act,fan_on,fan_off,count, ...
-                   HUM_avg,h_max,h_min,hum_act,hum_on,hum_off, ...
-                   t_h,time_up,time_down,intensity_of_light,light_int, ...
-                   light_act, light_on,light_off,options)
+function anomalies(T_avg,t_max,fan_S,fan_on,fan_off,count,HUM_avg, ...
+                  h_max,h_min,hum_S,hum_on,hum_off,t_h,time_up, ...
+                  time_down,light_val,light_int,light_S,light_on,light_off)
+
+% Extract a number from structure
+fan_act = fan_S.value;
+hum_act = hum_S.value;
+light_act = light_S.value;
 
 %% FAN CONTROL ANOMALIES
 
 % Internal temperature of Vesna is increased, the fan does not respond.
 if T_avg >= t_max && fan_act == fan_off && count < 120
-    try
-        email2('fan1');
-    catch
-        options = errors('email');
-    end
+    anomaly_D('fan1');
 end
+
 % Internal temperature of Vesna is normal, the fan does respond.
 if T_avg < t_max && fan_act == fan_on && count < 120
-    try
-        email2('fan2');
-    catch
-        options = errors('email');
-    end
+    anomaly_D('fan2');
 end
+
 % Vesna ventilation is malfunctioning.
 if count >= 120 && count < 135 && fan_act == fan_off
-    try
-        email2('vent');
-    catch
-        options = errors('email');
-    end
+    anomaly_D('vent');
 end
 
 %% PUMP CONTROL ANOMALIES
 
 % Internal humidity of Vesna is increased, the pump does respond.
 if HUM_avg >= h_max && hum_act == hum_on && count < 120
-    try
-        email2('pump1');
-    catch
-        options = errors('email');
-    end
+    anomaly_D('pump1');
 end
+
 % Internal humidity of Vesna is decreased, the pump does not respond.
 if HUM_avg <= h_min && hum_act == hum_off && count < 120
-    try
-        email2('pump2');
-    catch
-        options = errors('email');
-    end
+    anomaly_D('pump2');
 end
 
 %% LIGHTING CONTROL ANOMALIES
 
 % Lighting is switched on during the day with sufficient light intensity.
-if t_h >= time_down && t_h < time_up && intensity_of_light > light_int && light_act == light_on
-    try
-        email2('light1');
-    catch
-        options = errors('email');
-    end
-end
-% Lighting is switched off during the day with insufficient light intensity.
-if t_h >= time_down && t_h < time_up && intensity_of_light <= light_int && light_act == light_off
-    try
-        email2('light2');
-    catch
-        options = errors('email');
-    end
-end
-% Lighting is switched off during the day with insufficient light intensity.
-if (t_h < time_down && t_h >= time_up) && light_act == light_on
-    try
-        email2('night');
-    catch
-        options = errors('email');
-    end
+if t_h >= time_down && t_h < time_up && light_val > light_int && light_act == light_on
+    anomaly_D('light1');
 end
 
-% Rewrite options
-optionsN = options;
+% Lighting is switched off during the day with insufficient light intensity.
+if t_h >= time_down && t_h < time_up && light_val <= light_int && light_act == light_off
+    anomaly_D('light2');
+end
+
+% Lighting is switched off during the day with insufficient light intensity.
+if (t_h < time_down && t_h >= time_up) && light_act == light_on
+    anomaly_D('night');
+end
 
 end
