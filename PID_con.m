64d7c38295b03c74a0e44156f599fcaee16a7b69
Jozef Vargan <xvarganj@stuba.sk>
2022-12-27 20:53:37 +0100

    Fix (control error): Fixed updated control error in k-1 period, enhance documentation.

--- PID_con.m
+++ PID_con.m
@@ -3,45 +3,43 @@
 %
 % PID_con
 %
-% PID temperature controller of the Vesna greenhouse. M-file consists of
-% a function that provides a new value of control input 'u', as well as
-% values of control input input and error in k-1 period (including control
-% error in k-2 period) as output parameters. It requires a series of input
-% parameters to set-up current temperature control input.
+% File for setting the control input of the PID controller. M-file consists
+% of a function that provides the series of control output parameters. It
+% requires a series of input parameters to set control input.
 %
 % List of input variables
-%   e             - current control error.
-%   e_p1          - control error in in k-1 sampling period.
-%   e_p2          - control error in in k-2 sampling period.
-%   u_p1          - control input in in k-1 sampling period.
-%   Z_r           - proportional gain of the PID controller.
-%   T_i           - integral gain of the PID controller.
-%   T_d           - derivative gain of the PID controller.
-%   T_s           - sampling period (in minutes).
+%   e             - control error in k period
+%   e_p1          - control error in k-1 period
+%   e_p2          - control error in k-2 period
+%   T_d           - derivative gain
+%   T_i           - integral gain
+%   T_s           - sampling period (in minutes)
+%   u_p1          - control input in k-1 period
+%   Z_r           - proportional gain
 %
 % List of output variables
-%   u             - current control input.
-%   u_pN1         - new control input in in k-1 sampling period.
-%   e_pN1         - new control error in in k-1 sampling period.
-%   e_pN2         - new control error in in k-2 sampling period.
+%   e_pN1         - update control error in k-1 period
+%   e_pN2         - update control error in k-2 period
+%   u             - control input in k period
+%   u_pN1         - update control input in k-1 period
 %
 % List of local variables
-%   u_d           - control input difference (control increase).
+%   u_d           - control input difference
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function [u,u_pN1,e_pN1,e_pN2] = PID_con(e,e_p1,e_p2,u_p1,Z_r,T_i,T_d,T_s)
 
-% Reset control error & output
+% Update control error & output
 u_pN1 = u_p1;
 e_pN2 = e_p1;
-e_pN1 = e_p;
+e_pN1 = e_p2;
 
-% Control output
+% Temperature control input
 u_d = Z_r*(e-e_p1 + T_s/T_i*e + T_d/T_s*(e-2*e_p1+e_p2));
 u = u_p1 + u_d;
 
-% Control output saturation
+% Control input saturation
 if u > 255
     u = 255;
 elseif u < 0
