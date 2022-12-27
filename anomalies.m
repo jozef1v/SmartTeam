5addb1e5287cb674e48e6a08ac954c6206adfb65
Jozef Vargan <xvarganj@stuba.sk>
2022-12-27 12:04:38 +0100

    Feat (control reset): Reset control input for detected anomaly, added soil irrigation anomaly detection, enhance documentation.

--- anomalies.m
+++ anomalies.m
@@ -3,99 +3,137 @@
 %
 % ANOMALIES
 %
-% File for detecting the anomalies that have arisen. M-file consists of
-% a function that does not provide any output parameter. It requires 
-% a series of input parameters that are used to detect unexpected
-% errors (anomalies) associated with Vesna control.
+% File for detecting anomalies that arose during the control of the Vesna
+% greenhouse. M-file consists of a function that does not provide any
+% output parameter. It requires a series of input parameters that are used
+% to detect unexpected control behaviour (anomalies) associated with Vesna
+% control.
 %
 % List of used functions
-%   anomaly_d     - send notification e-mail. It requires specific error
+%   anomaly_D     - send notification e-mail. It requires specific error
 %                   identificator.
+%   send_data     - reset control input. It requires a series of input
+%                   data, specified to prevent the detected anomaly ('n'
+%                   stands for 'not used').
 %
 % List of input variables
-%   count         - iteration cycle number.
-%   t_h           - current time hour.
-%   time_up       - time to switch off daylight lighting and switch to
-%                   night temperature control.
-%   time_down     - time to switch on daylight lighting and switch to day
-%                   temperature control.
-%   light_on      - turn on the light.
-%   light_off     - turn off the light.
-%   light_int     - minimum permitted light intensity.
-%   t_max         - maximum permitted temperature in the greenhouse
-%                   (regulated by a fan).
-%   fan_on        - turn on the fan.
-%   fan_off       - turn off the fan.
-%   h_max         - maximum permitted humidity in the greenhouse
-%                   (regulated by humidifier).
-%   h_min         - minimum permitted humidity in the greenhouse
-%                   (regulated by humidifier).
-%   hum_on        - turn on the humidifier.
-%   hum_off       - turn off the humidifier.
-%   light_val     - light intensity.
-%   light_S       - control input for on/off lighting control.
-%   t_val         - average value of the temperature in th greenhouse.
-%   hum_S         - control input for on/off humidity control.
-%   hum_val       - average value of the humidity in the greenhouse.
-%   fan_S         - control input for on/off ventilation control.
+%   count         - Vesna control iteration period
+%   fan_off       - turn off the fan
+%   fan_on        - turn on the fan
+%   fan_S         - ventilation control input
+%   HUM_avg       - humidity mean value
+%   h_max         - maximum preferred humidity
+%   h_min         - minimum preferred humidity
+%   hum_off       - turn off the humidifier
+%   hum_on        - turn on the humidifier
+%   hum_S         - humidity control input
+%   irr_S         - irrigation control input
+%   light_int     - minimum preferred light intensity
+%   light_off     - turn off the light
+%   light_on      - turn on the light
+%   light_S       - lighting control input
+%   light_val     - light intensity
+%   soil_hum      - soil humidity
+%   soil_min      - minimum preferred soil humidity
+%   T_avg         - temperature mean value
+%   temp_S        - temperature control input
+%   t_h           - current time hour
+%   time_down     - daytime control start
+%   time_up       - night-time control start
+%   t_max         - maximum preferred temperature
+%   vent_duration - ventilation duration
+%   vent_start    - ventilation period
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function anomalies(T_avg,t_max,fan_S,fan_on,fan_off,count,HUM_avg, ...
-                  h_max,h_min,hum_S,hum_on,hum_off,t_h,time_up, ...
-                  time_down,light_val,light_int,light_S,light_on,light_off)
+    h_max,h_min,hum_S,hum_on,hum_off,t_h,time_up,time_down,light_val, ...
+    light_int,light_S,light_on,light_off,temp_S,irr_S,soil_hum, ...
+    vent_start,vent_duration,soil_min)
 
-% Extract a number from structure
-fan_act = fan_S.value;
-hum_act = hum_S.value;
-light_act = light_S.value;
+%% HEATER CONTROL ANOMALIES
+if T_avg >= t_max && temp_S ~= 0
+    send_data('n',0,'n','n','n','n','n','n')
+    anomaly_D('temp1');
+end
 
 %% FAN CONTROL ANOMALIES
 
-% Internal temperature of Vesna is increased, the fan does not respond.
-if T_avg >= t_max && fan_act == fan_off
-    anomaly_d('fan1');
+% Temperature is increased over maximum preferred value,
+% but the fan is turned off
+if T_avg >= t_max && fan_S == fan_off
+    send_data('n','n','n',fan_on,'n','n','n','n');
+    anomaly_D('fan1');
 end
 
-% Internal temperature of Vesna is normal, the fan does respond.
-if T_avg < t_max && fan_act == fan_on && count < 120
-    anomaly_d('fan2');
+% Temperature is below maximum preferred value,
+% but the fan is turned on (not ventilation time)
+if T_avg < t_max && fan_S == fan_on && count < vent_start
+    send_data('n','n','n',fan_off,'n','n','n','n');
+    anomaly_D('fan2');
 end
 
-% Vesna ventilation is malfunctioning.
-if count >= 120 && count < 135 && fan_act == fan_off
-    anomaly_d('vent');
+% It is time to ventilate, but the fan is turned off
+if count >= vent_start && count < (vent_start+vent_duration) && ...
+        fan_S == fan_off
+    send_data('n','n','n',fan_on,'n','n','n','n');
+    anomaly_D('vent');
 end
 
 %% PUMP CONTROL ANOMALIES
 
-% Internal humidity of Vesna is increased, the pump does respond.
-if HUM_avg >= h_max && hum_act == hum_on
-    anomaly_d('pump1');
+% Humidity is increased over maximum preferred value,
+% but the pump is turned on
+if HUM_avg >= h_max && hum_S == hum_on
+    send_data('n','n',hum_off,'n','n','n','n','n');
+    anomaly_D('pump1');
 end
 
-% Internal humidity of Vesna is decreased, the pump does not respond.
-if HUM_avg <= h_min && hum_act == hum_off
-    anomaly_d('pump2');
+% Humidity is decreased below minimum preferred value,
+% but the pump is turned off
+if HUM_avg <= h_min && hum_S == hum_off
+    send_data('n','n',hum_on,'n','n','n','n','n');
+    anomaly_D('pump2');
 end
 
 %% LIGHTING CONTROL ANOMALIES
 
-% Lighting is switched on during the day with sufficient light intensity.
-if t_h >= time_down && t_h < time_up && light_val > light_int && ...
-        light_act == light_on
-    anomaly_d('light1');
+% Light intensity is over minimum preferred value (during the daytime
+% control), but the lighting is turned on
+if t_h >= time_down && t_h < time_up && light_val >= light_int && ...
+        light_S == light_on
+    send_data(light_off,'n','n','n','n','n','n','n');
+    anomaly_D('light1');
 end
 
-% Lighting is switched off during the day with insufficient light intensity.
-if t_h >= time_down && t_h < time_up && light_val <= light_int && ...
-        light_act == light_off
-    anomaly_d('light2');
+% Light intensity is below minimum preferred value (during the daytime
+% control), but the lighting is turned off
+if t_h >= time_down && t_h < time_up && light_val < light_int && ...
+        light_S == light_off
+    send_data(light_on,'n','n','n','n','n','n','n');
+    anomaly_D('light2');
 end
 
-% Lighting is switched off during the day with insufficient light intensity.
-if (t_h < time_down && t_h >= time_up) && light_act == light_on
-    anomaly_d('night');
+% The lighting is turned on (during the night-time control)
+if (t_h < time_down && t_h >= time_up) && light_S == light_on
+    send_data(light_off,'n','n','n','n','n','n','n');
+    anomaly_D('night');
+end
+
+%% PLANT IRRIGATION CONTROL ANOMALIES
+
+% Soil humidity is over minimum preferred value,
+% but the irrigator is turned on
+if irr_S == 1 && soil_hum >= soil_min
+    send_data('n','n','n','n','n','n','n',0);
+    anomaly_D('irrig1');
+end
+
+% Soil humidity is below minimum preferred value,
+% but the irrigator is turned off
+if irr_S == 0 && soil_hum < soil_min
+    send_data('n','n','n','n','n','n','n',1);
+    anomaly_D('irrig2');
 end
 
 end
