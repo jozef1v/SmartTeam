5b395401c89deec61c4848dbc081114e03298989
Jozef Vargan <xvarganj@stuba.sk>
2022-12-27 13:54:48 +0100

    Refactor (load credentials): Remove unnecessary credentials load, enhance documentation.

--- credentials2.m
+++ credentials2.m
@@ -3,27 +3,20 @@
 %
 % CREDENTIALS2
 % 
-% File to load user send e-mail credentials (from external file
-% credentials2.txt). M-file consists of a function that does not require
-% input parameters. It provides the 'data' parameter which contains the
-% basic user e-mail credentials.
+% File to load e-mail credentials (from external file credentials2.txt).
+% M-file consists of a function that does not require any input parameters.
+% It provides credentials parameter which contains the basic sender's and
+% receiver's e-mail credentials.
 %
 % List of output variables
-%   data          - credentials to send an e-mail (sender's & recipient's
-%                   e-mails, sender's e-mail password).
+%   data          - send e-mail credentials (sender's & recipient's
+%                   e-mails, sender's e-mail password)
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function data = credentials2
 
-% Load set-up
-DELIMITER = ' ';
-HEADERLINES = 6;
-
-% Load data
-dataR = importdata("credentials2.txt", DELIMITER, HEADERLINES);
-[~,name] = fileparts("credentials2.txt");
-dataM.(matlab.lang.makeValidName(name)) = dataR;
-data = {dataM.credentials2{2};dataM.credentials2{4};dataM.credentials2{6}};
+data = importdata("credentials2.txt");
+data = {data{2};data{4};data{6}};
 
 end
