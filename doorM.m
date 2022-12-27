af4005b5633fb887cb47a7054db1fa05f046c72d
Jozef Vargan <xvarganj@stuba.sk>
2022-12-27 15:21:47 +0100

    Fix (control interrupt): Switched off irrigation control, enhance documentation.

--- doorM.m
+++ doorM.m
@@ -3,45 +3,41 @@
 %
 % doorM
 %
-% Vesna greenhouse door position detection file. M-file consists of
-% a function that provides the command 'skip' to turn off the control in
-% the greenhouse as an output parameter. It requires an input parameter
-% 'door_val' which numerically indicates the position of the door. If
-% the door is ajar, in such case the control is switched to stand-by mode -
-% temperature, humidity and ventilation control are switched off, light
-% stays on.
+% File for detecting the Vesna greenhouse door position. M-file consists of
+% a function that provides the command to manipulate control loop based on
+% door position in the greenhouse as an output parameter. It requires
+% an input parameter which indicates the position of the door. If the door
+% is open, in such case the control is switched to stand-by mode -
+% temperature, humidity, ventilation and irrigation control input are set
+% to zero, light is not affected.
 %
 % List of used functions
-%   send_data     - send data to Arduino API Cloud - control inputs, door
-%                   position.
-%   email2        - send notification e-mail about detected opened door. It
-%                   requires email 'door' id parameter.
+%   send_data     - send interrupt control data to Arduino API Cloud
+%   email2        - send notification e-mail. It requires 'door' error
+%                   identificator
 %
 % List of input variables
-%   door_val      - door opening position.
+%   door_val      - door opening position
 %
 % List of output variables
-%   skip          - command to turn off the Vesna regulation (activated by
-%                   the presence of an open door).
+%   skip          - manipulate Vesna control loop
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function skip = doorM(door_val)
 
-% Security during the open house (only light is functioning)
-if door_val
+% Door control setup
+if door_val == 1
 
     % Interrupt control loop
-    send_data(0,0,0,0,0,0,1);
-
-    % Send notification email
-    email('open');
+    send_data('n',0,0,0,'n','n','n',0);
+    email2('door');
 
     % Skip control loop
     skip = 1;
 
 else
-    
+
     % Unskip control loop
     skip = 0;
 
