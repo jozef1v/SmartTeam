c33579b30795e0d0d0b0a35984608dc9c6edc89d
Jozef Vargan <xvarganj@stuba.sk>
2022-12-28 11:53:34 +0100

    Refactor (send data): Send data are modified to be structures, enhance documentation.

--- write_data.m
+++ write_data.m
@@ -4,31 +4,28 @@
 % WRITE_DATA
 %
 % File for sending control input to Arduino API Cloud. M-file consists of
-% a function that does not provide an output parameter. It requires a
+% a function that does not provide any output parameters. It requires a
 % series of input parameters which contain identifiers and credentials to
 % send control input to the server.
 %
 % List of used functions
-%   errors        - check the type of error that occurred. If an error
-%                   occurs, it tries to resolve it and informs the user. It
-%                   requires error 'error_key' and 'spec' parameters.
+%   errors        - checks the type of error that occurred
 %
 % List of input variables
-%   pid           - identifier of the used device.
-%   id            - identifier of the device type.
-%   data          - control input data (structure type).
-%   error_key     - identifier of the error that occurred.
-%   options       - settings to connect to the Arduino API Cloud.
+%   data          - data value to send to Arduino API Cloud
+%   error_key     - identifier of the error type
+%   id            - identifier of the demanded device type
+%   options       - settings to connect to the Arduino API Cloud
+%   pid           - identifier of the demanded device
 %
 % List of local variables
-%   spec          - specifies to send an e-mail and display the error to
-%                   the Command Window.
+%   spec          - e-mail send attempts
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function write_data(pid,id,data,error_key,options)
 
-% Initialize counter 'spec' & request method
+% Initialize counter & request method
 spec = 0;
 options.RequestMethod = 'put';
 
@@ -37,11 +34,12 @@ while(true)
     try
         webwrite(strcat("https://api2.arduino.cc/iot/v2/things/{", ...
             device(pid),"}/properties/{",d_type(id),"}/publish"), ...
-            data,options);
+            struct('value',data),options);
         break
     catch
         pause(5)
         spec = spec + 1;
+
         % Terminates after 5 attempts
         if ~mod(spec,5)
             options = errors(error_key,spec);
